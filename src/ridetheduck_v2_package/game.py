233678c714aafccb60bd539a02add4563f7e10e4
Braeden Sy Tan <braedenjairsytan@icloud.com>
2026-08-28 23:03:07 +1200

    Added multi os support to redos

--- src/ridetheduck_v2_package/game.py
+++ src/ridetheduck_v2_package/game.py
@@ -1,4 +1,5 @@
 import pygame
+import platform
 import random
 import json
 import base64
@@ -265,7 +266,8 @@ class game_variable: # Game variables
         self.CMDhold = False
 
         self.gamecount = 0
-
+        self.platformChange = platform.system()
+        
         self.chipChangeHistory = []
         self.duckPositions = []
 
@@ -1052,7 +1054,7 @@ class game_functions():
                 elif event.unicode == 'x':
                     GV.shortcut[1] = False
 
-            if event.type == pygame.KEYDOWN and event.unicode == "z" and GV.chipChangeHistory and GV.CMDhold:
+            if event.type == pygame.KEYDOWN and ((GV.platformChange != "Linux" and event.unicode == "z") or (GV.platformChange == "Linux" and event.unicode == "y")) and GV.chipChangeHistory and GV.CMDhold:
                 for value in GV.chipChangeHistory.copy():
                     if ((GV.chipData[(value[1])[0]])[(value[1])[1]])["override"]:
                         GV.chipChangeHistory.remove(value)
@@ -1072,11 +1074,17 @@ class game_functions():
 
                     GV.chipChangeHistory.reverse()
                 
-                
-            if event.type == pygame.KEYDOWN and event.scancode == 227:
-                GV.CMDhold = True
-            elif event.type == pygame.KEYUP and event.scancode == 227:
-                GV.CMDhold = False
+            if GV.platformChange == "Darwin":
+                if event.type == pygame.KEYDOWN and event.scancode == 227:
+                    print("work")
+                    GV.CMDhold = True
+                elif event.type == pygame.KEYUP and event.scancode == 227:
+                    GV.CMDhold = False
+            elif GV.platformChange is "Windows" or GV.platformChange == "Linux":
+                if event.type == pygame.KEYDOWN and event.scancode == 224:
+                    GV.CMDhold = True
+                elif event.type == pygame.KEYUP and event.scancode == 224:
+                    GV.CMDhold = False
 
                 
     
