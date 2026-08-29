d93c16d60ea8477902c3113fcc9fa533f8add478
Braeden Sy Tan <braedenjairsytan@icloud.com>
2026-08-29 22:04:57 +1200

    Added quick chip button selection when exchanging

--- src/ridetheduck_v2_package/game.py
+++ src/ridetheduck_v2_package/game.py
@@ -264,6 +264,7 @@ class game_variable: # Game variables
         self.gameStartHover = [20, 20, 20]
         self.shortcut = [False, False] # exchange betting
         self.CMDhold = False
+        self.SHIFThold = False
 
         self.gamecount = 0
         self.platformChange = platform.system()
@@ -1055,7 +1056,6 @@ class game_functions():
                     GV.shortcut[1] = False
 
             if event.type == pygame.KEYDOWN and GV.chipChangeHistory and ((GV.CMDhold and ((GV.platformChange == "Darwin" and event.unicode == "z") or (GV.platformChange == "Linux" and event.unicode == "y"))) or event.unicode == "\x1a"):
-                print("wok")
                 for value in GV.chipChangeHistory.copy():
                     if ((GV.chipData[(value[1])[0]])[(value[1])[1]])["override"]:
                         GV.chipChangeHistory.remove(value)
@@ -1086,39 +1086,69 @@ class game_functions():
                 elif event.type == pygame.KEYUP and event.scancode == 224:
                     GV.CMDhold = False
 
-                
+            if event.type == pygame.KEYDOWN and (event.scancode == 225 or event.scancode == 229):
+                GV.SHIFThold = True
+            elif event.type == pygame.KEYUP and (event.scancode == 225 or event.scancode == 229):
+                GV.SHIFThold = False
     
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if int(list(reversed(GV.chipValues))[GV.exchangeChipSelection]) < GV.chipExchangeValue2 or (int(list(reversed(GV.chipValues))[GV.exchangeChipSelection]) == GV.chipExchangeValue2 and len(GV.chipExchange)!= 1):
                     if int(list(reversed(GV.chipValues))[GV.exchangeChipSelection]) <= GV.chipExchangeValue2-GV.chipExchangeValue1:
                         if event.button == 1:
                             if GV.chipExchangehighlightOn:
+                                if GV.SHIFThold:
+                                    if (GV.chipExchangeValue2-GV.chipExchangeValue1) % int(list(reversed(GV.chipValues))[GV.exchangeChipSelection]) == 0:
+                                        GV.clickupSFX.play(1)
+                                        add_value = int((GV.chipExchangeValue2-GV.chipExchangeValue1)/int(list(reversed(GV.chipValues))[GV.exchangeChipSelection]))
 
-                                GV.clickupSFX.play(0)
+                                        GV.chipSmallExchangeListtemp.reverse()
+                                        GV.chipSmallExchangeListtemp[GV.exchangeChipSelection] += add_value
+                                        GV.chipSmallExchangeListtemp.reverse()
 
-                                GV.chipSmallExchangeListtemp.reverse()
-                                GV.chipSmallExchangeListtemp[GV.exchangeChipSelection] += 1
-                                GV.chipSmallExchangeListtemp.reverse()
-                                GV.chipExchangeValue1 = 0
+                                        GV.chipExchangeValue1 = 0
 
-                                for indexexclist, value in enumerate(reversed(GV.chipSmallExchangeListtemp)):
-                                    if value > 0:
-                                        GV.chipExchangeValue1 += value * int(list(reversed(GV.chipValues))[indexexclist])
-                                GV.chipExchangeStr1 = (f"{GV.chipExchangeValue1:,}")
+                                        for indexexclist, value in enumerate(reversed(GV.chipSmallExchangeListtemp)):
+                                            if value > 0:
+                                                GV.chipExchangeValue1 += value * int(list(reversed(GV.chipValues))[indexexclist])
+                                        GV.chipExchangeStr1 = (f"{GV.chipExchangeValue1:,}")
+
+
+                                else:
+                                    GV.clickupSFX.play(0)
+
+                                    GV.chipSmallExchangeListtemp.reverse()
+                                    GV.chipSmallExchangeListtemp[GV.exchangeChipSelection] += 1
+                                    GV.chipSmallExchangeListtemp.reverse()
+                                    GV.chipExchangeValue1 = 0
+
+                                    for indexexclist, value in enumerate(reversed(GV.chipSmallExchangeListtemp)):
+                                        if value > 0:
+                                            GV.chipExchangeValue1 += value * int(list(reversed(GV.chipValues))[indexexclist])
+                                    GV.chipExchangeStr1 = (f"{GV.chipExchangeValue1:,}")
                 if event.button == 3:
                     if GV.chipExchangehighlightOn:
                         GV.chipSmallExchangeListtemp.reverse()
                         if GV.chipSmallExchangeListtemp[GV.exchangeChipSelection] > 0:
+                            if GV.SHIFThold:
+                                GV.clickdownSFX.play(1)
+                                
+                                GV.chipSmallExchangeListtemp[GV.exchangeChipSelection] = 0
+                                GV.chipExchangeValue1 = 0
 
-                            GV.clickdownSFX.play(0)
+                                for indexexclist, value in enumerate(GV.chipSmallExchangeListtemp):
+                                    if value > 0:
+                                        GV.chipExchangeValue1 += value * int(list(reversed(GV.chipValues))[indexexclist])
+                                GV.chipExchangeStr1 = (f"{GV.chipExchangeValue1:,}")
+                            else:
+                                GV.clickdownSFX.play(0)
 
-                            GV.chipSmallExchangeListtemp[GV.exchangeChipSelection] -= 1
-                            GV.chipExchangeValue1 = 0
+                                GV.chipSmallExchangeListtemp[GV.exchangeChipSelection] -= 1
+                                GV.chipExchangeValue1 = 0
 
-                            for indexexclist, value in enumerate(GV.chipSmallExchangeListtemp):
-                                if value > 0:
-                                    GV.chipExchangeValue1 += value * int(list(reversed(GV.chipValues))[indexexclist])
-                            GV.chipExchangeStr1 = (f"{GV.chipExchangeValue1:,}")
+                                for indexexclist, value in enumerate(GV.chipSmallExchangeListtemp):
+                                    if value > 0:
+                                        GV.chipExchangeValue1 += value * int(list(reversed(GV.chipValues))[indexexclist])
+                                GV.chipExchangeStr1 = (f"{GV.chipExchangeValue1:,}")
                         GV.chipSmallExchangeListtemp.reverse()
                 if event.button == 1:
 
